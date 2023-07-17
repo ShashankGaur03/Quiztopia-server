9c7d132d64905b6a97198b6bf143b46e7592baba
ShashankGaur03 <shashankgaur1308@gmail.com>
2023-07-17 14:40:07 +0530

    Seventh commit

--- index.js
+++ index.js
@@ -175,6 +175,6 @@ app.get("/",(req,res)=>{
 //     }).catch(err=>console.log(err));
 // })
 
-// app.listen(process.env.PORT || 5000,()=>{
-//     console.log(process.env.PORT);
-// })
+app.listen(process.env.PORT || 5000,()=>{
+    console.log(process.env.PORT);
+})
